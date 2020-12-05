f3e1d70ee347e4976db2974ff8163780d621728b
SwaggerHub <SwaggerHub@smartbear.com>
2020-12-05 04:55:54 +0000

    API definition transferred by SwaggerHub

---
+++ spring/src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -0,0 +1,44 @@
+package io.swagger.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-12-05T04:55:53.952Z[GMT]")
+@Configuration
+public class SwaggerDocumentationConfig {
+
+    @Bean
+    public Docket customImplementation(){
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                    .apis(RequestHandlerSelectors.basePackage("io.swagger.api"))
+                    .build()
+                .directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class)
+                .apiInfo(apiInfo());
+    }
+    @Bean
+    public OpenAPI configure() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("My Inventory API")
+                .description("This is a simple API")
+                .termsOfService("")
+                .version("1.0.0")
+                .license(new License()
+                    .name("Apache 2.0")
+                    .url("http://www.apache.org/licenses/LICENSE-2.0.html"))
+                .contact(new Contact()
+                    .email("sibendu@your-company.com")));
+    }
+
+}
